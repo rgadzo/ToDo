7127910dc02f2261ac458bf5838abbb34432068f
rgadzo <rgadzo@yahoo.com>
2021-05-25 17:47:49 +0200

    New frontend

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "todoapp"
 urlpatterns = [
+    path('new-task', views.task, name='task'),
     path('', views.home, name='home'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+
+def task(request):
+    return render(request, 'newtask.html')
