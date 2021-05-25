89f947d4b3cc87c835c9a53f2e78bb55f5806b5f
rgadzo <rgadzo@yahoo.com>
2021-05-25 18:37:54 +0200

    Added basic functionality

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import DateField
 
-# Create your models here.
+
+class Task(models.Model):
+    task_text = models.CharField(max_length=200)
+    task_time_added = models.DateField('date time')
+
+    def __str__(self):
+        return self.task_text
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from . import models
+from django.utils import timezone
 
 
 def home(request):
-    return render(request, 'base.html')
+    task_list_load_on_home = models.Task.objects.all()
+    context = {
+        'task_list_load_on_home': task_list_load_on_home
+    }
+    return render(request, 'base.html', context)
 
 
 def task(request):
-    return render(request, 'newtask.html')
+    task = request.POST.get('task')
+    models.Task.objects.create(task_text=task, task_time_added=timezone.now())
+    task_list = models.Task.objects.all()
+    context = {
+        'task_list': task_list,
+    }
+    return render(request, 'newtask.html', context)
