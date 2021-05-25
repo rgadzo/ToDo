179a5bb731519e708633bd4fa8758c8c38c5943c
rgadzo <rgadzo@yahoo.com>
2021-05-25 16:31:18 +0200

    Initial commit

---
+++ ToDo/urls.py
@@ -0,0 +1,8 @@
+from django import urls
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('todoapp.urls')),
+    path('admin/', admin.site.urls),
+]
