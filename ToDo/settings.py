614053a23ee1043a433ab0a16ec79566c1f20850
rgadzo <rgadzo@yahoo.com>
2021-05-26 16:30:23 +0200

    Added whitenoise for static files handling

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -25,14 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h9-v5g*0xy&@x7v#was!am7b3n!wk(hmq&(#h-rs!q2xto7al@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.35', ]
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'todoapp.apps.TodoappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Task
 
+admin.site.site_header = "ToDo Admin"
+admin.site.site_title = "ToDo Admin"
+admin.site.index_title = "Welcome to Admin panel"
+
 admin.site.register(Task)
